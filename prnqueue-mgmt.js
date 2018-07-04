efea113a376918a16ca74c91a107f5e96e888bf1
Sendil Kumar <sendilcareer@gmail.com>
2018-07-04 17:41:44 +0530

    added few more windows methods

--- prnqueue-mgmt.js
+++ prnqueue-mgmt.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const fs = require('fs');
 const process  = require('process');
 const windows = require('./lib/windows');
 
@@ -30,10 +31,35 @@ class Prnqueue{
       console.log('un-supported environment')
     }
   }
+  addTCPIPPort() {
 
-  addPrintQueue(queueName) {
+  }
+  addPrintQueue(queueName,portName,driverName) {
     //adds the printer
-    console.log('addPrintQueue');
+    if(this.osName === 'windows') {
+      windows.addPrinter(queueName,portName,driverName);
+    } else if(this.osName === 'mac') {
+      //lpstat -p
+    }
+    else if(this.osName === 'linux') {
+      //lpstat -p
+    }
+    else {
+      console.log('un-supported environment')
+    }
+  }
+  addPrintDriver(infPath, driverName, overWrite) {
+
+    if(overWrite === 1) {
+      //call add driver
+      addDriver(infPath, driverName);
+    } else {
+      //check and call
+      if(driverInstalled(driverName) !== 1){
+        //add driver
+        addDriver(infPath, driverName);
+      }
+    }
   }
 
   deletePrintQueue(queueName) {
@@ -79,6 +105,28 @@ class Prnqueue{
   }
 
 }
+function addDriver(infPath, driverName) {
+  if(fs.existsSync(infPath)) {
+    var arch = '';
+    if (this.osArch === 'x64') {
+      arch = "Windows x64";
+    }  else {
+       arch = "Windows NT x86";
+    }
+      windows.addDriver(infPath, driverName, arch);
+  }
+
+}
+
+function driverInstalled(driverName) {
+  var installed = 0;
+  var driverList = MyLibrary.EnumDrivers();
+  if (driverList.toLowerCase().indexOf(driverName.toLowerCase()) !== -1) {
+    installed = 1;
+    console.log("Driver already installed");
+  }
+}
+
 function identifyArch(osArch){
 
  /*Supported architectures: 'arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x32', and 'x64'*/
--- test.js
+++ test.js
@@ -4,13 +4,16 @@ const prnqueue = new Prnqueue(); //create the object
 //call the log method
 //prnqueue.log('messgae');
 
-prnqueue.listPrintQueues();
+//prnqueue.listPrintQueues();
 
 //prnqueue.setDefaultPrintQueue('Xerox Global Print Driver PS');
 
 //prnqueue.getDefaultPrintQueue();
 
-//prnqueue.deletePrintQueue('bgl12-00-b1-6-cx');
+//prnqueue.deletePrintQueue('printer1');
 
-prnqueue.addPrintQueue('printer',);
+prnqueue.addTCPIPPort();
+prnqueue.addPrintDriver('c:\\Windows\\Drivers\\Printers\\Xerox_GPD_PS\\x2UNIVP.inf', 'Xerox Global Print Driver PS', 1);
+
+prnqueue.addPrintQueue('test123','10.20.25.35','Xerox Global Print Driver PS');
 
