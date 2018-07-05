08976978e1cbb564c40ba586aa8aa299c2a3acb1
Sendil Kumar <sendilcareer@gmail.com>
2018-07-05 19:04:53 +0530

    windows methods completed

--- prnqueue-mgmt.js
+++ prnqueue-mgmt.js
@@ -1,7 +1,10 @@
 const os = require('os');
-const fs = require('fs');
-const process  = require('process');
+
 const windows = require('./lib/windows');
+const mac = require('./lib/mac');
+const linux = require('./lib/linux');
+const common = require('./lib/common');
+
 
 class Prnqueue{
 
@@ -10,15 +13,8 @@ class Prnqueue{
     this.osArch = identifyArch(os.arch());
   }
 
-  log(message) {
-    console.log(message);
-    console.log(this.osName);
-    console.log(this.osArch);
-
-  }
-
   listPrintQueues() {
-    //list the printers
+
     if(this.osName === 'windows') {
       windows.listPrinters();
     } else if(this.osName === 'mac') {
@@ -28,70 +24,12 @@ class Prnqueue{
       //lpstat -p
     }
     else {
-      console.log('un-supported environment')
-    }
-  }
-  addTCPIPPort() {
-
-  }
-  addPrintQueue(queueName,portName,driverName) {
-    //adds the printer
-    if(this.osName === 'windows') {
-      windows.addPrinter(queueName,portName,driverName);
-    } else if(this.osName === 'mac') {
-      //lpstat -p
-    }
-    else if(this.osName === 'linux') {
-      //lpstat -p
-    }
-    else {
-      console.log('un-supported environment')
-    }
-  }
-  addPrintDriver(infPath, driverName, overWrite) {
-
-    if(overWrite === 1) {
-      //call add driver
-      //console.log('inside addPrintDriver');
-      addDriver(infPath, driverName,this.osArch);
-    } else {
-      //check and call
-      if(driverInstalled(driverName) !== 1){
-        //add driver
-        addDriver(infPath, driverName,this.osArch);
-      }
-    }
-  }
-
-  deletePrintQueue(queueName) {
-    //deletes the printer
-    if(this.osName === 'windows') {
-      windows.deletePrinter(queueName);
-    } else if(this.osName === 'mac') {
-      //lpadmin -d
-    }
-    else if(this.osName === 'linux') {
-      //lpadmin -d
-    }
-    else {
-      console.log('un-supported environment')
-    }
-  }
-  setDefaultPrintQueue(queueName) {
-    if(this.osName === 'windows') {
-      windows.setDefaultPrinter(queueName);
-    } else if(this.osName === 'mac') {
-      //lpadmin -d
-    }
-    else if(this.osName === 'linux') {
-      //lpadmin -d
-    }
-    else {
-      console.log('un-supported environment')
+      console.log('un-supported environment');
     }
   }
 
   getDefaultPrintQueue() {
+
     if(this.osName === 'windows') {
       windows.getDefaultPrinter();
     } else if(this.osName === 'mac') {
@@ -105,67 +43,73 @@ class Prnqueue{
     }
   }
 
-}
-function addDriver(infPath, driverName, osArch) {
-  if(fs.existsSync(infPath)) {
-    var arch = '';
-   // console.log(osArch);
-    if (osArch === 'x64') {
+  setDefaultPrintQueue(queueName) {
 
-      arch = "Windows x64";
-    }  else {
-       arch = "Windows NT x86";
+    if(this.osName === 'windows') {
+      windows.setDefaultPrinter(queueName);
+    } else if(this.osName === 'mac') {
+      //lpadmin -d
     }
-    //console.log('inside addDriver');
-    //console.log(infPath);
-    //console.log(driverName);
-    //console.log(arch);
-      windows.addDriver(infPath, driverName, arch);
-      //console.log('called  windows.addDriver');
+    else if(this.osName === 'linux') {
+      //lpadmin -d
+    }
+    else {
+      console.log('un-supported environment')
+    }
   }
 
-}
+  deletePrintQueue(queueName) {
 
-function driverInstalled(driverName) {
-  var installed = 0;
-  var driverList = MyLibrary.EnumDrivers();
-  if (driverList.toLowerCase().indexOf(driverName.toLowerCase()) !== -1) {
-    installed = 1;
-    console.log("Driver already installed");
+    if(this.osName === 'windows') {
+      windows.deletePrinter(queueName);
+    } else if(this.osName === 'mac') {
+      //lpadmin -d
+    }
+    else if(this.osName === 'linux') {
+      //lpadmin -d
+    }
+    else {
+      console.log('un-supported environment')
+    }
   }
+
+  addPrintQueue(prnQueueOptions) {
+
+    if(this.osName === 'windows') {
+      windows.addPrinter(prnQueueOptions, this.osArch);
+    } else if(this.osName === 'mac') {
+      //lpstat -p
+    }
+    else if(this.osName === 'linux') {
+      //lpstat -p
+    }
+    else {
+      console.log('un-supported environment');
+    }
+
+  }
+
 }
 
 function identifyArch(osArch){
-
- /*Supported architectures: 'arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x32', and 'x64'*/
-  var value = 'un-supported';
-
-  if(osArch === 'x64') {
-    value = osArch;
-  } else if(osArch==='x32') {
-    value = 'x86';
-  }
-  return value;
+  /*'arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x32', and 'x64'*/
+   return ((osArch === 'x64') ? osArch : ((osArch ==='x32')? 'x86' : 'un-supported'));
 }
 
 function identifyOS(osName) {
-   /*'aix'
-    'darwin'
-    'freebsd'
-    'linux'
-    'openbsd'
-    'sunos'
-    'win32'*/
-  var value = 'un-supported';
 
-  if(osName === 'darwin') {
-    value = 'mac';
-  } else if(osName === 'win32'){
-    value = 'windows';
-  } else if( osName === 'aix' || osName === 'freebsd' || osName === 'linux' || osName === 'openbsd' || osName === 'sunos') {
-    value = 'linux';
-  }
-  return value;
-}
+    /*'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'*/
+   var value = 'un-supported';
+   if(osName === 'darwin') {
+     value = 'mac';
+   } else if(osName === 'win32'){
+     value = 'windows';
+   } else if( osName === 'aix' || osName === 'freebsd' || osName === 'linux' || osName === 'openbsd' || osName === 'sunos') {
+     value = 'linux';
+   }
+   return value;
+ }
 
 module.exports = Prnqueue;
+
+
--- test.js
+++ test.js
@@ -1,20 +1,34 @@
 const Prnqueue = require('./prnqueue-mgmt');//require the class
 const prnqueue = new Prnqueue(); //create the object
 
-//call the log method
-//prnqueue.log('messgae');
+const prnQueueOptions = {
 
-//prnqueue.listPrintQueues();
+  //Queue configuration
+  queueName: 'test',
 
-//prnqueue.setDefaultPrintQueue('Xerox Global Print Driver PS');
+  //Driver configuration - windows only
+  infAbsPath: 'c:\\Windows\\Drivers\\Printers\\Xerox_GPD_PS\\x2UNIVP.inf',
+  driverName: 'Xerox Global Print Driver PS',
+  driverOverwrite: 1,
 
-//prnqueue.getDefaultPrintQueue();
+  //Port configuration, IP Address or Hostname
+  portName: '10.123.145.64'
+};
 
-//prnqueue.deletePrintQueue('printer1');
+prnqueue.listPrintQueues();
 
-//prnqueue.addTCPIPPort();
-//prnqueue.addPrintDriver('c:\\Windows\\Drivers\\Printers\\Xerox_GPD_PS\\x2UNIVP.inf', 'Xerox Global Print Driver PS', 1);
-prnqueue.addPrintDriver('C:\\HP Universal Print Driver\\ps-x64-6.6.0.23029\\hpcu215v.inf', 'HP Universal Printing PS', 1);
+prnqueue.addPrintQueue(prnQueueOptions);
 
-prnqueue.addPrintQueue('test123','10.20.25.35','HP Universal Printing PS');
+prnqueue.setDefaultPrintQueue('test');
+
+prnqueue.getDefaultPrintQueue();
+
+prnqueue.deletePrintQueue('test');
+
+prnqueue.setDefaultPrintQueue('Send To OneNote 16');
+
+prnqueue.listPrintQueues();
+
+
+
 
