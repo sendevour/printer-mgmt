755e7139d2cdfa9da6cb8c4d20695b93a7472560
Sendil Kumar <sendilcareer@gmail.com>
2018-07-06 12:35:34 +0530

    started multi os support implementation

--- prnqueue-mgmt.js
+++ prnqueue-mgmt.js
@@ -1,89 +1,68 @@
 const os = require('os');
 
-const windows = require('./lib/windows');
-const mac = require('./lib/mac');
-const linux = require('./lib/linux');
 const common = require('./lib/common');
 
+var osIdentity = '';
 
 class Prnqueue{
 
   constructor(osName,osArch) {
     this.osName = identifyOS(os.platform());
     this.osArch = identifyArch(os.arch());
+
+    if(this.osName === 'windows') {
+      osIdentity = require('./lib/windows');
+
+    } else if(this.osName === 'mac') {
+      osIdentity = require('./lib/mac');
+    }
+    else if(this.osName === 'linux') {
+      osIdentity = require('./lib/linux');
+    }
+
   }
 
   listPrintQueues() {
 
-    if(this.osName === 'windows') {
-      windows.listPrinters();
-    } else if(this.osName === 'mac') {
-      //lpstat -p
-    }
-    else if(this.osName === 'linux') {
-      //lpstat -p
-    }
-    else {
+    if(this.osName !== 'un-supported') {
+      osIdentity.listPrinters();
+    } else {
       console.log('un-supported environment');
     }
   }
 
   getDefaultPrintQueue() {
 
-    if(this.osName === 'windows') {
-      windows.getDefaultPrinter();
-    } else if(this.osName === 'mac') {
-      //lpstat -d
-    }
-    else if(this.osName === 'linux') {
-      //lpstat -d
-    }
-    else {
-      console.log('un-supported environment')
+    if(this.osName !== 'un-supported') {
+      osIdentity.getDefaultPrinter();
+    } else {
+      console.log('un-supported environment');
     }
   }
 
   setDefaultPrintQueue(queueName) {
 
-    if(this.osName === 'windows') {
-      windows.setDefaultPrinter(queueName);
-    } else if(this.osName === 'mac') {
-      //lpadmin -d
-    }
-    else if(this.osName === 'linux') {
-      //lpadmin -d
-    }
-    else {
+    if(this.osName !== 'un-supported') {
+      osIdentity.setDefaultPrinter(queueName);
+    } else {
       console.log('un-supported environment')
     }
   }
 
   deletePrintQueue(queueName) {
 
-    if(this.osName === 'windows') {
-      windows.deletePrinter(queueName);
-    } else if(this.osName === 'mac') {
-      //lpadmin -d
-    }
-    else if(this.osName === 'linux') {
-      //lpadmin -d
-    }
-    else {
+    if(this.osName !== 'un-supported') {
+      osIdentity.deletePrinter(queueName);
+    } else {
       console.log('un-supported environment')
     }
   }
 
   addPrintQueue(prnQueueOptions) {
 
-    if(this.osName === 'windows') {
-      windows.addPrinter(prnQueueOptions, this.osArch);
-    } else if(this.osName === 'mac') {
-      //lpstat -p
-    }
-    else if(this.osName === 'linux') {
-      //lpstat -p
-    }
-    else {
+    if(this.osName !== 'un-supported') {
+      osIdentity.addPrinter(prnQueueOptions, this.osArch);
+    } else {
       console.log('un-supported environment');
     }
 
